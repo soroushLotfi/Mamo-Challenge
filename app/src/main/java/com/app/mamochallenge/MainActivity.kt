773ec097b62b2388df9e5939e5f5ee56ff6702d4
Soroush Lotfi <soroush80lotfi@gmail.com>
2021-08-10 20:11:30 +0430

    feature/enteringNumber   ===>   :main screen ui done

--- app/src/main/java/com/app/mamochallenge/MainActivity.kt
+++ app/src/main/java/com/app/mamochallenge/MainActivity.kt
@@ -3,11 +3,7 @@ package com.app.mamochallenge
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import com.app.mamochallenge.ui.mainScreen.MainScreen
 import com.app.mamochallenge.ui.theme.MamoChallengeTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,24 +11,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MamoChallengeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                MainScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MamoChallengeTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/app/mamochallenge/ui/mainScreen/MainScreen.kt
@@ -0,0 +1,77 @@
+package com.app.mamochallenge.ui.mainScreen
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun MainScreen() {
+    val keyboardItemsList: List<String> = remember {
+        val list = mutableListOf<String>()
+        for (number in 1..9) {
+            list.add(number.toString())
+        }
+        list.apply {
+            add(".")
+            add("0")
+            add("⌫")
+        }
+    }
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            Text(
+                modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+                text = "123",
+                fontSize = 48.sp
+            )
+        }
+        Spacer(modifier = Modifier.height(24.dp))
+        LazyVerticalGrid(
+            cells = GridCells.Fixed(3)
+        ) {
+            items(keyboardItemsList) { item ->
+                Box(
+                    modifier = Modifier.height(104.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    OutlinedButton(
+                        modifier = Modifier
+                            .height(80.dp)
+                            .width(80.dp),
+                        shape = CircleShape,
+                        onClick = {
+
+                        }
+                    ) {
+                        Text(
+                            text = item,
+                            fontSize = 24.sp,
+                            color = MaterialTheme.colors.primary,
+                        )
+                    }
+                }
+            }
+        }
+        Spacer(modifier = Modifier.height(12.dp))
+    }
+}
