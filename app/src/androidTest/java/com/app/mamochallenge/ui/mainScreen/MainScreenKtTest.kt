4277cb57196d6566c5a00249654a6e02ef60617a
Soroush Lotfi <soroush80lotfi@gmail.com>
2021-08-13 12:24:30 +0430

    feature/enteringNumber   ===>   :get expected text from string resources in ui tests

--- app/src/androidTest/java/com/app/mamochallenge/ui/mainScreen/MainScreenKtTest.kt
+++ app/src/androidTest/java/com/app/mamochallenge/ui/mainScreen/MainScreenKtTest.kt
@@ -1,11 +1,14 @@
 package com.app.mamochallenge.ui.mainScreen
 
+import android.content.Context
 import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.filters.MediumTest
+import com.app.mamochallenge.R
 import com.app.mamochallenge.ui.theme.MamoChallengeTheme
 import org.junit.Before
 import org.junit.Rule
@@ -34,13 +37,17 @@ class MainScreenKtTest {
 
     @Test
     fun testNumberInitialValue() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val expectedText = context.getString(R.string.price, "0.00")
         composeTestRule
             .onNodeWithContentDescription("NumberText")
-            .assertTextEquals("0.00")
+            .assertTextEquals(expectedText)
     }
 
     @Test
     fun testKeyboardKeys() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val expectedText = context.getString(R.string.price, "1,755,900.05")
         composeTestRule.onNodeWithText("1").performClick()
         composeTestRule.onNodeWithText("7").performClick()
         composeTestRule.onNodeWithText("5").performClick()
@@ -55,7 +62,7 @@ class MainScreenKtTest {
         composeTestRule.onNodeWithText("5").performClick()
         composeTestRule
             .onNodeWithContentDescription("NumberText")
-            .assertTextEquals("1,755,900.05")
+            .assertTextEquals(expectedText)
     }
 
 }
